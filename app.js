01a09f34239f4cc4cc9c966bd1446cc5be131390
wandi1209 <wandider@gmail.com>
2025-02-20 19:42:44 +0700

    create connection project with mongodb

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require("express");
+require("dotenv").config();
+
 const addMovie = require("./controllers/addMovie");
+
+const mongoose = require("mongoose");
+
+// Connection to mongodb
+mongoose
+  .connect(process.env.MONGO_CONNECTION, {})
+  .then(() => {
+    console.log("db connected!");
+  })
+  .catch(() => {
+    console.log("db failed to connect!");
+  });
 
 const app = express();
 
